3f1f0aa7b16d56392509a54ce27504d1bfe7bec1
Alon Braymok <alon@ludus.ac>
2020-05-04 11:25:10 +0300

    before side menu installtion

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,11 +1,17 @@
 import React, { useState, useLayoutEffect } from "react";
 import { useDispatch, useSelector, batch } from "react-redux";
 import Navbar from "../../components/homepage/navbar/navbar";
+import { TasksList } from "../../components/tasks.list/tasks.list/tasks.list";
 
 export function Homepage({ history }) {
   const user = useSelector(({ userReducer }) => userReducer.user);
 
   const dispatch = useDispatch();
 
-  return <Navbar user={user} push={history.push} />;
+  return (
+    <>
+      <Navbar user={user} push={history.push} />
+      <TasksList push={history.push} />
+    </>
+  );
 }
